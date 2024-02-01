4315c57892d9b39280d30a48aee9094040927bbf
Paulo <pdichone@gmail.com>
2024-01-31 16:50:15 -0800

    Refactor BillAmountField

--- lib/main.dart
+++ lib/main.dart
@@ -89,20 +89,17 @@ class _UTipState extends State<UTip> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
+              padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(5),
                   border:
                       Border.all(color: theme.colorScheme.primary, width: 2)),
               child: Column(
                 children: [
-                  TextField(
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.attach_money),
-                        labelText: 'Bill Amount'),
-                    keyboardType: TextInputType.number,
-                    onChanged: (String value) {
-                      print("Value: $value ");
+                  BillAmountField(
+                    billAmount: "100",
+                    onChanged: (value) {
+                      print("Amount: $value");
                     },
                   ),
                   // Split Bill area
